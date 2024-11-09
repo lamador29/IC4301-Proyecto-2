6710f9f7c1abdbe184434de4bf3d23a23609874b
Laura Amador <vane29111@gmail.com>
2024-11-08 21:44:40 -0600

    Update mapReduce.py

--- mapReduce/mapReduce.py
+++ mapReduce/mapReduce.py
@@ -1,6 +1,5 @@
 from mrjob.job import MRJob
 import csv
-# pip install mrjob
 
 class primero(MRJob):
     def mapper(self, _, line):
@@ -16,5 +15,59 @@ class primero(MRJob):
     def reducer(self, word, links):
         yield word, list(set(links))
 
+class segundo(MRJob):
+    def mapper(self, _, line):
+        reader = csv.reader([line])
+        try:
+            link, title, num_words, text = next(reader)
+        except ValueError:
+            return
+        
+        words = text.split()
+        for i in range(len(words) - 1):
+            pair = (words[i].lower(), words[i + 1].lower())
+            yield pair, link
+
+    def reducer(self, pair, links):
+        yield pair, list(set(links))
+
+
+class tercero(MRJob):
+    def mapper(self, _, line):
+        reader = csv.reader([line])
+        try:
+            link, title, num_words, text = next(reader)
+        except ValueError:
+            return
+        
+        word_counts = {}
+        for word in text.split():
+            word = word.lower()
+            word_counts[word] = word_counts.get(word, 0) + 1
+        
+        for word, count in word_counts.items():
+            yield (link, word), count
+
+    def reducer(self, link_word, counts):
+        yield link_word, sum(counts)
+
+class cuarto(MRJob):
+    def mapper(self, _, line):
+        reader = csv.reader([line])
+        try:
+            link, title, num_words, text = next(reader)
+        except ValueError:
+            return
+        
+        for word in text.split():
+            yield word.lower(), 1
+
+    def reducer(self, word, counts):
+        yield word, sum(counts)
+
 if __name__ == '__main__':
-    primero.run()
+    #primero.run()
+    segundo.run()
+    #tercero.run()
+    #cuarto.run()
+
